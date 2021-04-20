2e452942015896ea3326bd194ddad3dcbb5aa08a
Elise Tran <tran0592@umn.edu>
2021-04-20 17:56:30 -0500

    Allowing single task view and single project view

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <tr>
+        <td>{{task.name}}</td>
+        <td>{{task.deadline}}</td>
+        <td>{{task.description}}</td>
+        <td><router-link :to="{name:'SingleTask', params:{id:this.id}}" id="todosSingle">More About Task</router-link></td>
+    </tr>
+</template>
+
+<script> 
+
+export default {
+    name: "task",
+    props: ["id","task","project_id"],
+}
+</script>
+<style scoped>
+    td {
+        padding: 1em;
+        color: black;
+    }
+</style>
--- src/views/ProjectSpecific.vue
+++ src/views/ProjectSpecific.vue
@@ -9,24 +9,37 @@
         <td>{{project_object.project_name}}</td>
       </tr>
     </table>
+    <br>
+    <table>
+      <tr>
+        <th><u><b>Task Name</b></u></th>
+        <th><u><b>Deadline</b></u></th>
+        <th><u><b>Task Details</b></u></th>
+        <th><u><b>Specific Task View</b></u></th>
+      </tr>
+      <task v-for="task in allToDosFromProject" :key="task.id" :id = "task.id" :task = "task" :project_id = "project_id"/>
+    </table>
   </div>
 </template>
 
 <script>
 import {db} from "../firebaseConfig.js"
+import task from "../components/ToDoItem.vue"
 export default {
   name: "ProjectSpecific",
-  components: {
+  components: {task
   },
   data: function () {
     return {
       project_idLocal: this.$route.params.project_id,
-      project_object: null
+      project_object: null, 
+      allToDosFromProject: []
     };
   },
   firestore: function() {
         return {
-            project_object: db.collection("projects").doc(this.project_idLocal)
+            project_object: db.collection("projects").doc(this.project_idLocal),
+            allToDosFromProject: db.collection("to-do-items").where("project_id", "==", this.project_idLocal)
         }
     },
 
@@ -40,5 +53,14 @@ export default {
     border-block-style: double;
     border-color: lightseagreen;
     font-size: 2em;
+  }
+
+  tr {
+    padding: 0.5em;
+    color: black;
+  }
+
+  th {
+    padding: 0.5em;
   }
 </style>
