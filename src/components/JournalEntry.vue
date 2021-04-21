1247db85901aac133528be11aa09fcf82b3c8414
Jessie Speert <speer034@umn.edu>
2021-04-20 18:14:38 -0700

    Implemented infrastructure for the the rest of the pages, specifically the journal entry portions of our application

---
+++ src/components/JournalEntry.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    //import { db } from "../firebaseConfig.js"
+
+    export default {
+        name: "task",
+        props: ["id", "entry", "project_id"],
+        methods: {
+
+        }
+    }
+</script>
