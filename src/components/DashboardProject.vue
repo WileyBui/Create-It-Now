34d51d4455821745c8c5771e52c0d08d202bee1a
Wiley Bui <17buih@gmail.com>
2021-04-20 20:32:48 -0500

    Added router requirements for unauthorized users accessing authorized contents

--- src/components/DashboardProject.vue
+++ src/components/DashboardProject.vue
@@ -2,29 +2,37 @@
   <div class="accordion accordion-flush mb-3" id="accordionFlushExample">
     <div class="accordion-item green-background">
       <h2 class="accordion-header" id="flush-headingOne">
-          <button class="accordion-button show green-background"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#flush-collapseOne"
-                  aria-expanded="true"
-                  aria-controls="flush-collapseOne">
-              <router-link :to="{
+        <button
+          class="accordion-button show green-background"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#flush-collapseOne"
+          aria-expanded="true"
+          aria-controls="flush-collapseOne"
+        >
+          <router-link
+            :to="{
               name: 'ProjectSpecific',
               params: { project_id: project.id },
             }"
-                           class="remove-a-href-styles text-dark">
-              <strong>{{ project.project_name }}&nbsp; </strong>
-              <template v-if="complete">
-                  <strong class="Complete"> - Complete </strong>
-              </template>
-              <template v-else>
-                  <strong class="Incomplete"> - In Progress </strong>
-              </template>
+            class="remove-a-href-styles text-dark"
+          >
+            <strong>{{ project.project_name }} - </strong>
+            <template v-if="complete">
+              <strong class="Complete">COMPLETED </strong>
+            </template>
+            <template v-else>
+              <strong class="Incomplete">IN PROGRESS </strong>
+            </template>
 
-              <button type="button" class="btn blue-background color-white p-1 pt-0 pb-0">View</button>
-
-              </router-link>
-          </button>
+            <button
+              type="button"
+              class="btn blue-background color-white p-1 pt-0 pb-0"
+            >
+              View
+            </button>
+          </router-link>
+        </button>
       </h2>
       <div
         id="flush-collapseOne"
@@ -33,7 +41,7 @@
         data-bs-parent="#accordionFlushExample"
       >
         <div class="accordion-body">
-          <strong>ToDo Items:</strong>
+          <strong>To-do Items:</strong>
           <DashboardTask v-for="task in tasks" :key="task.id" :task="task" />
         </div>
       </div>
@@ -53,20 +61,23 @@ export default {
   },
   data: function () {
     return {
-        tasks: [],
-        complete: false
+      tasks: [],
+      complete: false,
     };
   },
 
   created() {
-      var doneTasks = db.collection("to-do-items").where("project_id", "==", this.project.id).where("isComplete", "==", false);
-      doneTasks.get().then((res) => {
-          if (res.size == 0) {
-              this.complete = true;
-          } else {
-              this.complete = false;
-          }
-      })
+    var doneTasks = db
+      .collection("to-do-items")
+      .where("project_id", "==", this.project.id)
+      .where("isComplete", "==", false);
+    doneTasks.get().then((res) => {
+      if (res.size == 0) {
+        this.complete = true;
+      } else {
+        this.complete = false;
+      }
+    });
   },
 
   firestore: function () {
@@ -93,11 +104,10 @@ export default {
 }
 
 strong.Complete {
-    color: lawngreen;
+  color: green;
 }
 
 strong.Incomplete {
-    color: yellow;
+  color: yellow;
 }
-
 </style>
