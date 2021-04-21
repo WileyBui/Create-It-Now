7624172da8302b67aae9493078264daffa447b47
Ally Goins <goin0004@umn.edu>
2021-04-21 10:36:08 -0500

    fix https

--- src/views/SingleTask.vue
+++ src/views/SingleTask.vue
@@ -28,7 +28,7 @@
             </tr>
             <tr>
                     <td>{{taskName.description}}</td>
-                    <td>{{ taskName.deadline }}</td>
+                    <td>{{DateUtils.format(taskName.deadline,'MMMM Do YYYY, h:mm:ss a') }}</td>
                     <template v-if="taskName.isComplete">
                         <td>
                             <button
