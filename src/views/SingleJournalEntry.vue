dded53259375d0f983e288f687aa38ae2a96456a
Jessie Speert <speer034@umn.edu>
2021-04-21 13:30:56 -0700

    Added a basic structure to the single journal entry page; still needs a lot of polish, the ability to edit, and photo ability

---
+++ src/views/SingleJournalEntry.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>{{entry.title}}</h2>
+        <p>{{entry.description}}</p>
+    </div>
+</template>
+
+<script>
+import { db } from "../firebaseConfig.js"
+
+export default {
+    name: "SingleJournalEntry",
+    props: [],
+    data: function () {
+        return {
+            entry_idLocal: this.$route.params.id,
+            entry: null
+        }
+    },
+
+    firestore: function () {
+        return {
+            entry: db.collection("journalEntries").doc(this.entry_idLocal)
+        }
+    }
+}
+</script>
