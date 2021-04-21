46b1c385b930a18dc3f844ef91ff431e30aef416
Ally Goins <goin0004@umn.edu>
2021-04-21 12:41:48 -0500

    bah

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,13 @@
   <div class="about">
     <h1>This is an about page</h1>
     <modalCamera/>
+
   </div>
 </template>
 
 <script>
 import ModalCamera from '@/components/WebCamera.vue'
+
 
 export default {
   name: "About",
