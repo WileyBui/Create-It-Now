7df8e9ab092b191fc2cff31ae112c1960994626d
Wiley Bui <17buih@gmail.com>
2021-04-22 13:13:52 -0500

    Added second modal for Ally: not completed yet

--- src/components/ModalAddOrUpdateJournal.vue
+++ src/components/ModalAddOrUpdateJournal.vue
@@ -1,49 +1,89 @@
 <template>
-  <div
-    class="modal fade"
-    id="addAJournal"
-    tabindex="-1"
-    aria-labelledby="addAJournalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="addAJournalLabel">
-            <i class="bi bi-list-task"></i>
-            <!-- make a conditional statement if you're adding or updating a journal -->
-            <template v-if="journalEntry.length > 0"> Update Journal</template>
-            <template v-else> Add a Journal</template>
-          </h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body container text-start">
-          <label class="control-label w-100">Journal Title</label>
-          <input type="text" class="form-control" v-model="journal_title" />
+  <div>
+    <div
+      class="modal fade"
+      id="addAJournal"
+      tabindex="-1"
+      aria-labelledby="addAJournalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="addAJournalLabel">
+              <i class="bi bi-list-task"></i>
+              <!-- make a conditional statement if you're adding or updating a journal -->
+              <template v-if="journalEntry.length > 0">
+                Update Journal</template
+              >
+              <template v-else> Add a Journal</template>
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body container text-start">
+            <label class="control-label w-100">Journal Title</label>
+            <input type="text" class="form-control" v-model="journal_title" />
 
-          <label class="control-label w-100 margin-top-10">Description</label>
-          <textarea
-            class="form-control"
-            rows="3"
-            v-model="journal_description"
-          ></textarea>
+            <label class="control-label w-100 margin-top-10">Description</label>
+            <textarea
+              class="form-control"
+              rows="3"
+              v-model="journal_description"
+            ></textarea>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-bs-target="#modal2"
+              data-bs-toggle="modal"
+              data-bs-dismiss="modal"
+            >
+              Take a picture
+            </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="addANewJournal"
+            >
+              Submit
+            </button>
+          </div>
         </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Close
-          </button>
-          <button type="button" class="btn btn-primary" @click="addANewJournal">
-            Submit
-          </button>
+      </div>
+    </div>
+    <div
+      class="modal fade"
+      id="modal2"
+      aria-hidden="true"
+      aria-labelledby="..."
+      tabindex="-1"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <WebCamera />
+          <div class="modal-footer">
+            <!-- Toogle to first dialog, `data-bs-dismiss` attribute can be omitted - clicking on link will close dialog anyway -->
+            <a
+              class="btn btn-primary"
+              href="#modal"
+              data-bs-toggle="modal"
+              role="button"
+              >Open #modal</a
+            >
+          </div>
         </div>
       </div>
     </div>
@@ -52,11 +92,14 @@
 
 <script>
 import { db } from "../firebaseConfig.js";
+import WebCamera from "./WebCamera.vue";
 
 export default {
   name: "ModalAddOrUpdateJournal",
   props: ["todo_id", "project_id"],
-
+  components: {
+    WebCamera,
+  },
   data: function () {
     return {
       journal_title: null,
---
+++ src/components/ModalAddPicture.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="modal fade"
+    id="modal2"
+    aria-hidden="true"
+    aria-labelledby="..."
+    tabindex="-1"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        ...
+        <div class="modal-footer">
+          <!-- Toogle to first dialog, `data-bs-dismiss` attribute can be omitted - clicking on link will close dialog anyway -->
+          <a
+            class="btn btn-primary"
+            href="#modal"
+            data-bs-toggle="modal"
+            role="button"
+            >Open #modal</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
