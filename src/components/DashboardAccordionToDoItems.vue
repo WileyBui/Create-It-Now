17f4933ddbbd62b656669052046122f3d17499b2
Wiley Bui <17buih@gmail.com>
2021-04-23 03:28:36 -0500

    Chore: renamed 'accordian' to 'accordion'; AllJournalsFromProject should not display when there are no journals

--- src/components/DashboardAccordionToDoItems.vue
+++ src/components/DashboardAccordionToDoItems.vue
@@ -1,22 +1,22 @@
 <template>
   <div v-if="tasks.length > 0" class="accordion" id="to-do-accordion">
     <div class="accordion-item green-background">
-      <h2 class="accordion-header" id="headingAccordian">
+      <h2 class="accordion-header" id="headingAccordion">
         <button
-          class="accordion-button green-background color-black fw-bold"
+          class="accordion-button green-background color-black fw-bold showw"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#collapseToDoAccordian"
+          data-bs-target="#collapseToDoAccordion"
           aria-expanded="true"
-          aria-controls="collapseToDoAccordian"
+          aria-controls="collapseToDoAccordion"
         >
           To-do Items
         </button>
       </h2>
       <div
-        id="collapseToDoAccordian"
-        class="accordion-collapse collapse"
-        aria-labelledby="headingAccordian"
+        id="collapseToDoAccordion"
+        class="accordion-collapse collapse show"
+        aria-labelledby="headingAccordion"
         data-bs-parent="#to-do-accordion"
       >
         <div class="accordion-body">
@@ -31,7 +31,7 @@
 import DashboardTask from "./DashboardTask.vue";
 
 export default {
-  name: "DashboardToDoItemsAccordian",
+  name: "DashboardToDoItemsAccordion",
   props: ["tasks"],
   components: {
     DashboardTask,
